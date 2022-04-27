279cfa115cc863a7b3cd7284392ac58c49ddcc2f
suhyun <suuxxirr7@gmail.com>
2022-04-27 20:14:53 +0900

    practice2

---
+++ Assets/Scripts/Strings.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Strings : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {print("hello my name is Liam.");
+    print("hello".Length + "liam".Length); // 5 + 4
+    print("elephant".Substring(3,4)); // "phan"    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
